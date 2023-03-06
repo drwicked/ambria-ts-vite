10fa0156364d630ba74d82cd64ab461662e53cc8
Jeff P <yeffrey@gmail.com>
2023-03-06 03:11:46 +0000

    link updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import { useState } from 'react';
+import { AiOutlineAmazon, AiOutlineInstagram } from 'react-icons/ai';
+import { FaGoodreads } from 'react-icons/fa';
 import './App.css';
-import Header from '@/Components/Header';
-import ReadTheDocs from '@/Components/ReadTheDocs';
+
+const links = [
+  {
+    title: 'Amazon Author Page',
+    url: 'https://www.amazon.com/stores/Rachel-Anding/author/B00I6L1LT4',
+    Icon: AiOutlineAmazon,
+  },
+  {
+    title: 'Goodreads',
+    url: 'https://www.goodreads.com/author/show/7796202.Rachel_Anding',
+    Icon: FaGoodreads,
+  },
+  {
+    title: 'mothmanateee (art)',
+    url: 'https://www.instagram.com/mothmanateee/',
+    Icon: AiOutlineInstagram,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <Header />
-      <div className="card">
-        <button type="button" onClick={() => setCount(count + 1)}>
-          count is {count}
-        </button>
+      <h1 className="header">Ambria Eden</h1>
+      <div className="card-list">
+        {links.map(({ Icon, title, url }) => (
+          <div className="card" key={title}>
+            <a href={url}>
+              <Icon />
+              {title}
+            </a>
+          </div>
+        ))}
       </div>
-      <ReadTheDocs />
     </div>
   );
 }
